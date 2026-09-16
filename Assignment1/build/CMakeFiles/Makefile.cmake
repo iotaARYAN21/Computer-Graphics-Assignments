003f8db212dc9d6b2e6e23638bb5ff8972c259f1
Aryan <112301041@smail.iitpkd.ac.in>
2026-09-16 15:24:35 +0530

    updated .gitignore-aryan

--- Assignment1/build/CMakeFiles/Makefile.cmake
+++ Assignment1/build/CMakeFiles/Makefile.cmake
@@ -7,7 +7,7 @@ set(CMAKE_DEPENDS_GENERATOR "Unix Makefiles")
 # The top level Makefile was generated from the following files:
 set(CMAKE_MAKEFILE_DEPENDS
   "CMakeCache.txt"
-  "/home/sujit/CS5024/Computer-Graphics-Assignments/Assignment1/CMakeLists.txt"
+  "/home/iotaaryan/Documents/CGAssignments/Assignment1/CMakeLists.txt"
   "CMakeFiles/4.2.3/CMakeCCompiler.cmake"
   "CMakeFiles/4.2.3/CMakeCXXCompiler.cmake"
   "CMakeFiles/4.2.3/CMakeSystem.cmake"
